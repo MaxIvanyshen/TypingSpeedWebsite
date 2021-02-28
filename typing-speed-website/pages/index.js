d4a86b6b32701c77e3994e621dee240bacb4c71c
MaxIvanyshen <maxivanyshen@gmail.com>
2021-02-28 16:26:19 +0200

    need to implement showText() and clearInputField() function by spacebar

--- typing-speed-website/pages/index.js
+++ typing-speed-website/pages/index.js
@@ -1,11 +1,39 @@
 import { render } from "react-dom";
-import Image from 'next/image'
+import Image from 'next/image';
+import React from 'react';
+
+function clearInputField() {
+  $('#textInput').val(null);
+};
+
+function saveText() {
+  let text = $('#textInput').val();
+  showText(text);
+  clearInputField();
+}
+
+function showText(text) {
+  var arr = ["hello", "world", "my", "dear", "friend", "lovely"];
+  let colorParam = "color";
+  let color = "red";
+  for(let i=0; i<arr.length; i++) {
+    if(arr[i] == text) {
+      color = "green";
+      break;
+    }
+    else {
+      continue;
+    }
+  }
+  $('#showingText').html(text).css(colorParam, color);
+}
 
 export default function Home() {
   return(
     <html>
       <head>
-        <title>TypoingSpeed.io</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <title>TypingSpeed.io</title>
       </head>
       <body>
         <header>
@@ -20,15 +48,29 @@ export default function Home() {
               width={1920}
               height={248}
               objectFit="cover"
-            />
+            /> 
+            <h1 className="typeFaster">Type Faster!</h1>
           </div>
           <div className="mainContainer">
-            <div className="rectangle"></div>
-            <div className="smallRectangle"></div>
-            <div class="timer">
-              <div class="circle"></div>
+            <div className="rectangles">
+              <div className="rectangle"></div>
+              <div className="smallRectangle">
+                <h3 id="showingText"></h3>
+              </div>
+            </div>
+            <div className="timer">
+              <div className="circle"></div>
               <div id="countDown" class="countDown"></div>
-              <h1 class="timerLabel">timer</h1>
+              <h1 className="timerLabel">timer</h1>
+            </div>
+            <div className="wordsCounter">
+              <div className="wordsCounterCircle"></div>
+              <div id="countDown" class="countDown"></div>
+              <h1 className="counterLabel">words counter</h1>
+            </div>
+            <div className="inputContainer">
+              <input type="text" id="textInput" className="textInput"></input>
+              <button id="button" className="button" onClick={()=>saveText()}>clear</button>
             </div>
           </div>
         </div>
