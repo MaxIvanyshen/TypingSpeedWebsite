738054acd58a47047a857ed6b3f4d77a1fda48ea
MaxIvanyshen <70772808+MaxIvanyshen@users.noreply.github.com>
2021-03-21 11:46:02 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,181 @@
+import { render } from "react-dom";
+import Image from 'next/image';
+import React from 'react';
+
+const words = require("../public/text/words.json");
+
+var counter = 0, wrongCounter = 0, correctCounter = 0;
+let fullText="";
+let textToType="";
+let showed=0;
+let wordNumber=0;
+
+let arr = words.text.split(" ");
+let randomArr=[];
+
+function randomIndex(min, max) {
+  return min+Math.floor((max-min)*Math.random());
+}
+
+function nextText() {
+  if(showed==0) {
+
+    for(var i=0; i<arr.length; ++i) {
+      let r = randomIndex(1, arr.length);
+      let randomElem = arr[r];
+      textToType=textToType+randomElem+" ";
+      randomArr[i]=randomElem;
+    }
+    $("#showingText").html(null);
+    $("#showingText").html(textToType);
+    showed++;
+  }
+}
+
+function checkSpacePress() {
+  nextText();
+  $('#textInput').keydown((e)=>{
+    let code = e.code;
+    if(code == "Space") {
+      showText($('#textInput').val());
+      clearInputField();
+      e.preventDefault();
+    }
+  });
+}
+
+function clearInputField() {
+  $('#textInput').val(null);
+}
+
+function showText(text) {
+  let color = "red";
+  // for(let i=0; i<randomArr.length; ++i) {
+  //   if(text == randomArr[i]) {
+  //     color="green";
+  //     correctCounter++;
+  //     counter++;
+  //     break;
+  //   }
+  // }
+  if(text==randomArr[wordNumber]) {
+    color="green";
+    correctCounter++;
+    wordNumber++;
+    counter++;
+  }
+  if(color == 'red') {
+    counter++;
+    wrongCounter++;
+    wordNumber++;
+  }
+  if(counter==1) {
+    timer();
+  }
+  if(counter == randomArr.length) {
+    showed=0;
+    nextText();
+  }
+  if(counter>=10) {    
+    $('#counter').html(counter).css("color", "black");
+    $('#counter').html(counter).css("left", "228.5%");
+  }
+  if(counter>=100) {    
+    $('#counter').html(counter).css("color", "black");
+    $('#counter').html(counter).css("left", "219.25%");
+  }
+  else {
+    $('#counter').html(counter).css("color", "black");
+  }
+  console.log("Correct: " + correctCounter + "\n" + "Wrong: " + wrongCounter);
+  $(".typingText").html(text).css("color", color);
+}
+
+function timer() {
+  let time = 59;
+  setInterval(() => {
+    if(time>=10)
+      $('.timerCountDown').html("0:" + time);
+    if(time<10&&time>0)
+      $('.timerCountDown').html("0:0"+time);
+    if(time==0) {
+      $('.timerCountDown').html("0:00");
+      $("#counter").html("0").css("left", "237.5%");
+      finishCountDown();
+    }
+    time--;
+  },1000);
+}
+
+function finishCountDown() {
+  $("#textInput").css("disabled", "disabled");
+  calculateWPM();
+}
+
+function calculateWPM() {
+  let wpm = Math.floor((((counter/5) - wrongCounter)/1), 0);
+  console.log(wpm);
+  showResult(wpm);
+}
+
+function showResult(wpm) {
+  $(".arrow").css("display", "unset");
+}
+
+export default function Home(jsonFile) {
+  return(
+    <html>
+      <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <title>TypingSpeed.io</title>
+      </head>
+      <body>
+        <header>
+          <h2 className="headerName">
+            TypingSpeed.io
+          </h2>
+        </header>
+        <div className="posterContainer">
+          <div className="keyboard">
+            <Image
+              src="/mainImg.jpg"
+              width={1920}
+              height={248}
+              objectFit="cover"
+            /> 
+            <h1 className="typeFaster">Type Faster!</h1>
+          </div>
+          <div className="mainContainer">
+            <div className="rectangles">
+              <div className="rectangle"></div>
+              <div className="smallRectangle">
+                <h3 id="showingText"></h3>
+                <div className="showingTypingText">
+                  <h3 className="typingTextLabel">You typed: </h3>
+                  <h3 className="typingText"></h3>
+                </div>                
+              </div>
+            </div>
+            <div className="timer">
+              <div className="circle"></div>
+              <div className="timerCountDown">1:00</div>
+              <h1 className="timerLabel">timer</h1>
+            </div>
+            <div className="wordsCounter">
+              <div className="wordsCounterCircle"></div>
+              <div id="countDown" className="countDown"></div>
+              <h3 id="counter">0</h3>
+              <h1 className="counterLabel">words counter</h1>
+            </div>
+            <div className="inputContainer">
+              <input type="text" id="textInput" onClick={()=>checkSpacePress()} className="textInput"></input>
+            </div>
+            <div className="arrowContainer">
+              <div className="arrow bounce"></div>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
